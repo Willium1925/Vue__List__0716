1ab3fee6e8081d4fa0dc82e795ee3253bacc01a6
JIN <D1397026@o365.fcu.edu.tw>
2025-07-16 12:06:49 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 
 <template>
   <div id="app">
-    <h1>待辦清單</h1>
+
+    <h1>直接使用 ToDoItem 組件</h1>
     <ul>
       <li>
         <to-do-item label="安安你好，預設勾選" :done="true"></to-do-item>
@@ -10,17 +11,72 @@
         <to-do-item label="安安你好，預設不勾" :done="false"></to-do-item>
       </li>
     </ul>
+
+    <!-- 換行 -->
+    <br><br><br>
+
+    <h1>使用渲染</h1>
+    <ul>
+      <li v-for="item in ToDoItems00" :key="item.id">
+        <to-do-item
+            :label="item.label"
+            :done="item.done"
+            :id="item.id"></to-do-item>
+      </li>
+    </ul>
+
+
+
+
+
   </div>
 
 </template>
 <script>
 import ToDoItem from "./components/ToDoItem.vue";
+import ToDoForm from "@/components/ToDoForm.vue"; // 這裡來的資料是最內層的，要讓他出現在此
 
 export default {
   name: "app",
   components: {
     ToDoItem,
+    ToDoForm,
   },
+
+  data() {
+    return {
+      ToDoItems00: [
+        { label: "銀魂", done: true },
+        { label: "鏈鋸人", done: false },
+        { label: "進擊的巨人", done: true },
+        { label: "鬼滅之刃", done: false },
+        { label: "海賊王", done: true },
+        { label: "火影忍者", done: false },
+        { label: "名偵探柯南", done: true },
+        { label: "我的英雄學院", done: false },
+      ],
+
+    };
+  },
+  methods: {
+    // 這裡可以添加方法來處理待辦事項的新增、刪除等操作
+  },
+  mounted() {
+    // 在組件掛載後可以進行一些初始化操作
+    console.log("App 組件已掛載");
+    //
+  },
+  watch: {
+    // 可以監聽 ToDoItems00 的變化，進行相應的處理
+    ToDoItems00(newVal) {
+      console.log("待辦事項列表已更新:", newVal);
+    },
+  },
+  computed: {
+    // 可以添加計算屬性來處理待辦事項的統計等
+  },
+
+
 };
 </script>
 
---
+++ src/components/ToDoForm.vue
@@ -0,0 +1,39 @@
+<script>
+    // 一個輸入表單用來新增待辦事項
+    // label 提示要輸入什麼
+    // input 是文字輸入框 會有 id name 屬性
+    // 按鈕用來新增待辦事項
+    // 輸入的內容要跟 data 的 label 做雙向綁定
+    // 當按下新增按鈕時，會觸發 onSubmit 方法
+    // data 會有 label 屬性 用來儲存輸入的待辦事項
+import { ref } from 'vue';
+const newToDo = ref('');
+const addToDo = () => {
+  if (newToDo.value.trim() !== '') {
+    // 這裡可以添加邏輯來處理新增待辦事項
+    console.log('新增待辦事項:', newToDo.value);
+    newToDo.value = ''; // 清空輸入框
+  } else {
+    console.warn('待辦事項不能為空');
+  }
+};
+
+
+</script>
+
+<template>
+  一個輸入表單用來新增待辦事項
+  label 提示要輸入什麼input 是文字輸入框 會有 id name 屬性按鈕用來新增待辦事項
+  <form @submit.prevent="addToDo">
+    <label for="todo-input">新增待辦事項:</label>
+    <input type="text" id="todo-input" v-model="newToDo" name="todo-input" required />
+    <button type="submit">新增</button>
+  </form>
+
+
+
+</template>
+
+<style scoped>
+
+</style>
